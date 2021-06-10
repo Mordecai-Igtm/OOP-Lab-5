7434e1b770d02e2fa7a330da4adceb7e4eed0ed0
Mordecai-Igtm <isaaccooperhack@gmail.com>
2021-06-10 17:42:06 +0300

    OOP-Lab-5
    OOP-Lab-5

---
+++ ex26/ex26/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+string names = "Logan Mike Colin Ian Mia Alex Ava Nick Zoe William James";
+Array.ForEach(names.Split(' ').OrderByDescending(x => x.Length).ToArray(), x => Console.WriteLine(x));
---
+++ ex36/Ex36/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RuVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ruVowels = "АаЕеЁёИиОоУуЫыЭэЮюЯя";
+            string str;
+
+            Console.Write("Строка: ");
+            str = Console.ReadLine();
+
+            string[] words = str.Split(new Char[] { ' ', '\t', ',', '.', '!', '?' });
+            int cntWords = words.Length;
+            int[] counters = new int[cntWords];
+            for (int i = 0; i < cntWords; ++i)
+            {
+                for (int j = 0; j < words[i].Length; ++j)
+                    if (ruVowels.IndexOf(words[i][j]) != -1)
+                        counters[i]++;
+            }
+            int maxVowels = counters[0];
+            for (int i = 1; i < cntWords; ++i)
+                if (counters[i] > maxVowels)
+                    maxVowels = counters[i];
+
+            Console.WriteLine("Слово(а) з найбільшою кількістю голосних букв:");
+            for (int i = 0; i < cntWords; ++i)
+                if (counters[i] == maxVowels)
+                    Console.WriteLine(words[i]);
+
+            Console.ReadLine();
+        }
+    }
+}
